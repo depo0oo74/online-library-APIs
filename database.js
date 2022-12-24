0b6aa6f9d972710eb9938084c12a4a1b01168b3c
unknown <eldeeba029@gmail.com>
2022-12-25 01:34:48 +0200

    convert database to online

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
-const URL = "mongodb://localhost:27017/online-library";
+const DB_URL = `mongodb+srv://dep0oo74:JslzS5v0Vaw3Da8r@dep0oo74.mk0uu.mongodb.net/online-library?retryWrites=true&w=majority`;
 
 mongoose
-  .connect(URL)
+  .connect(DB_URL)
   .then(() => {
     console.log("database connected successfully");
   })
