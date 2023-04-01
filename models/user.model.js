3b41ae171bb974f2aad7bac1d86861345afd8c59
unknown <eldeeba029@gmail.com>
2023-04-02 01:00:29 +0200

    edit users model and controller

--- models/user.model.js
+++ models/user.model.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const userSchema = mongoose.Schema({
   username: String,
   email: String,
-  phoneNumber: Number,
+  phoneNumber: String,
   password: String,
   cpassword: String,
   addresses: [
