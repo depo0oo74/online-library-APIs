04443834804b26acac52dcae888a19d7caa3b249
Ahmed Eldeeb <eldeeba029@gmail.com>
2023-08-10 16:05:42 +0300

    change database url

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-const DB_URL = `mongodb+srv://dep0oo74:JslzS5v0Vaw3Da8r@dep0oo74.mk0uu.mongodb.net/online-library?retryWrites=true&w=majority`;
+const DB_URL = `mongodb+srv://dep0oo74:A372019a@dep0oo74.mk0uu.mongodb.net/`;
 
 mongoose
   .connect(DB_URL)
